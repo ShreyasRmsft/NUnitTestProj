a62765cd2d24c1a8e789ce2be304f5cc99adc25b
Shreyas R <shrer@microsoft.com>
2017-09-27 14:00:52 +0530

    abc

--- NUnitTestMseng/UnitTest1.cs
+++ NUnitTestMseng/UnitTest1.cs
@@ -6,10 +6,8 @@ namespace NUnitTestMseng
     [TestFixture]
     public class UnitTest1
     {
-        [TestCase(@"Space Test\space test.txt", @"Space Test\space test.txt")]
-        [TestCase(@"Space Test/space test.txt", @"Space Test/space test.txt")]
-        [TestCase(@"Assets\%20 test %20.txt", @"Assets\%2520 test %2520.txt")]
-        public void TestMethod1(string a, string b)
+        [TestCase(@"a\b")]
+        public void TestMethod1(string a)
         {
             
         }
